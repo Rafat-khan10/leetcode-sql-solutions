ff494988f2b88e7c2f8bf0170ea0c9e46ef1cce4
Rafat Khan <rafatkhan2210@gmail.com>
2026-04-08 22:17:36 +0600

    Add solution : find-total-time -spent-by-each-employee

---
+++ easy/find-total-time -spent-by-each-employee.sql
@@ -0,0 +1,13 @@
+-- Problem:  Find Total Time Spent by Each Employee
+-- Difficulty: Easy
+-- Source: Leetcode
+-- Link:https://leetcode.com/problems/find-total-time-spent-by-each-employee/description/
+
+-- Solution:
+
+SELECT
+ event_day AS day,
+ emp_id,
+ SUM(out_time-in_time) AS total_time 
+FROM Employees 
+GROUP BY event_day ,emp_id 
