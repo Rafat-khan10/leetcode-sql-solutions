60f782ae55f0827dc1c9a9179410d1923e61cda3
Rafat Khan <rafatkhan2210@gmail.com>
2026-07-22 01:36:10 +0600

    Add solution : actors-and-directors-who-cooperated-at-least-three-times.sql

---
+++ easy/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -0,0 +1,13 @@
+-- Problem: Actors and Directors Who Cooperated At Least Three Times
+-- Difficulty: Easy
+-- Source: LeetCode
+-- Link:https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/description/
+
+-- Solution:
+
+SELECT
+    actor_id,
+    director_id 
+FROM ActorDirector
+GROUP BY actor_id ,director_id 
+HAVING COUNT(*) >=3
